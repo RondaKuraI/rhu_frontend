07cf4453188c8bbd8402f6065874bd8ac98be249
RondaKuraI <johnronda0527@gmail.com>
2023-11-15 00:20:09 +0800

    sample table

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import IndexPage from '../views/IndexPage.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: IndexPage
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/IndexPage.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <table border="1">
+            <tr>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                <th>Email</th>
+                <th>Password</th>
+            </tr>
+            <tr v-for="users in users">
+                <td>{{ users.firstname }}</td>
+                <td>{{ users.lastname }}</td>
+                <td>{{ users.email }}</td>
+                <td>{{ users.password }}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default{
+    data(){
+        return{
+            users:[],
+        }
+    },
+    created(){
+        this.getUsers();
+    },
+    methods:{
+        async getUsers(){
+            try {
+                const user = await axios.get('getData');
+                this.users = user.data;
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+}
+</script>
