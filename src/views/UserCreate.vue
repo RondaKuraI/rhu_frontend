b631e982835f06fbc5773d25e5f49258c2df2f89
RondaKuraI <johnronda0527@gmail.com>
2023-11-16 22:14:35 +0800

    Update UserCreate.vue

--- src/views/UserCreate.vue
+++ src/views/UserCreate.vue
@@ -4,6 +4,24 @@
             <div class="card-header">
                 <h4>Add Users</h4>
             </div>
+            <div class="card-body">
+                <div class="mt-3">
+                    <label for="">First Name</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="mt-3">
+                    <label for="">Last Name</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="mt-3">
+                    <label for="">Email</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="mt-3">
+                    <label for="">Password</label>
+                    <input type="text" class="form-control">
+                </div>
+            </div>
         </div>
     </div>
 </template>
