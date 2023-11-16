2745d3b6374fb346aad0f00919cbcd98860b89b4
RondaKuraI <johnronda0527@gmail.com>
2023-11-16 22:06:58 +0800

    added UserCreate view

--- src/views/UserCreate.vue
+++ src/views/UserCreate.vue
@@ -1,3 +1,9 @@
 <template>
-    <h2>feefwfefwe</h2>
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-header">
+                <h4>Add Users</h4>
+            </div>
+        </div>
+    </div>
 </template>
